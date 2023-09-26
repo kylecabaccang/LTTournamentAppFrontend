88341954f4ee69660c20e4e1b08428c39f506b22
MSI\zedek <zedekiah.cabaccang@luckycue.tech>
2023-09-26 15:08:54 +0800

    16-player single elimination tournament bracket. Finished.

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,61 +1,25 @@
 import React, { useState } from 'react';
 
-export default function TournamentBracket() {
-  const [rnd1Match1Winner, setRnd1Match1Winner] = useState('#');
-  const [rnd1Match1Selected, setRnd1Match1Selected] = useState(false);
+function Match({ player1, player2, onWinnerSelected }) {
 
-  // Create an array of player names
-  const players = [];
-  for (let i = 1; i <= 16; i++) {
-    players.push(`Player ${i}`);
-  }
+  const [winner, setWinner] = useState(null);
+  const [loser, setLoser] = useState(null);
 
-  const handleWinnerClick = (player) => {
-    // Add logic to handle winner click and update state
+  const handleWinnerSelected = (player) => {
+    const loser = player === player1 ? player2 : player1;
+    setWinner(player);
+    setLoser(loser);
+    onWinnerSelected(player, loser);
   };
 
   return (
-    <div className="text-center">
-      <div className="grid grid-cols-3">
-         {/* Column 1: Players 1-16 */}
-         <div className="col-span-1">
-          {players.map((player, index) => (
-            <div key={index} className="">
-              <div
-                className={`border-2 border-black ${rnd1Match1Selected ? 'disabled' : ''}`}
-                onClick={() => handleWinnerClick(player)}>
-                {player}
-              </div>
-              {/* Add an empty cell with the same size as players' cells after every 2 players (excluding the last two) */}
-              {index % 2 === 1 && index < players.length - 2 && (
-                <div className={`border-2 border-transparent h-[40px]`}>d</div>
-              )}
-            </div>
-          ))}
-        </div>
+    <div className='p-2'>
+      <div className='border-black border-2'>
+        <button onClick={() => handleWinnerSelected(player1)}>{player1}</button>
+      </div>
 
-
-         {/* Column 3: Different boxes */}
-         <div className="col-span-1">
-          {players.map((player, index) => (
-            <div key={index}>
-              {/* Add empty cells with the same size as Box 1 and Box 2 */}
-              {index % 2 === 0 && index < players.length - 1 && (
-                <div>
-                  <div className="border-2 border-transparent">d</div>
-                  <div className="border-2 border-transparent">d</div>
-                </div>
-              )}
-              {/* Add different boxes here */}
-              {index % 2 === 0 && (
-                <div className="border-2 border-red-500">Box 1</div>
-              )}
-              {index % 2 === 0 && (
-                <div className="border-2 border-red-500">Box 2</div>
-              )}
-            </div>
-          ))}
-        </div>
+      <div className='border-black border-2'>
+        <button onClick={() => handleWinnerSelected(player2)}>{player2}</button>
       </div>
     </div>
   );
